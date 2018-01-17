5da696e3e07836eb3f4b37503fbcc4a7bc65b739
Doug Black <dblack@americanbible.org>
2018-01-17 11:45:23 -0500

    Initial db config

---
+++ config/db1.php
@@ -0,0 +1,8 @@
+<?php
+
+$db1 = array(
+	'servername' => '127.0.0.1',
+	'username' => 'root',
+	'password' => 'root',
+	'dbname' => 'db1'
+);
---
+++ config/db2.php
@@ -0,0 +1,8 @@
+<?php
+
+$db2 = array(
+	'servername' => '127.0.0.1',
+	'username' => 'root',
+	'password' => 'root',
+	'dbname' => 'db2'
+);
