31c048b5c37d7066615076c06ae4ae543bae5e7a
Doug Black <dblack@americanbible.org>
2018-01-22 08:07:07 -0500

    Comments on DB Config

--- config/db1.php
+++ config/db1.php
@@ -1,5 +1,10 @@
 <?php
 
+// This is the EE2 SENDING DATABASE!
+// The Entries will move from this Database to the other one.
+// Do NOT use on the actual DB until you are 100% ready to do so
+// Use on a copy until you get it right
+
 $db1 = array(
 	'servername' => '127.0.0.1',
 	'username' => 'root',
--- config/db2.php
+++ config/db2.php
@@ -1,5 +1,12 @@
 <?php
 
+// This is the EE4 RECEIVING DATABASE!
+// The Entries will move TO this Database FROM the other one.
+// Do NOT use on the actual DB until you are 100% ready to do so
+// Use on a copy until you get it right
+// ESPECIALLY ON THIS ONE
+// THIS WILL BE OVERWRITTEN!
+
 $db2 = array(
 	'servername' => '127.0.0.1',
 	'username' => 'root',
