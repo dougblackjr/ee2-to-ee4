f322d6eb1a1ba237c26763434df54d67f5974451
Doug Black <dblack@americanbible.org>
2018-01-22 08:09:50 -0500

    Import View, Start stylin

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
+<?php
+	define("DOCROOT", getcwd());
+	require_once(DOCROOT . 'controllers/ViewController.php');
+?>
+
 <!doctype html>
 <html lang="en">
 	<head>
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
 	</head>
 	<body>
+		<header class="row">
+			<div class="col-xs-12">Mover.</div>
+		</header>
+		<section>
+			
+		</section>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
 	</body>
 </html>
