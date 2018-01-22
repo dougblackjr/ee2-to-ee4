5ae9751cdb8f4b5722f5dc6f5dbe87b259ebc428
Doug Black <dblack@americanbible.org>
2018-01-22 08:18:46 -0500

    Moving PHP out of script

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
-<?php
-	define("DOCROOT", getcwd());
-	require_once(DOCROOT . 'controllers/ViewController.php');
-?>
-
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
+
 	<head>
-		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Mover.</title>
+		<meta name="author" content="Doug Black">
+
+		<!-- CSS -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<!-- script -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.13/vue.min.js"></script>
 	</head>
 	<body>
 		<header class="row">
@@ -15,6 +21,7 @@
 		<section>
 			
 		</section>
+		<!-- Bottom scripts -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
 	</body>
 </html>
